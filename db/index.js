3eefe05e7543cb5fefd60cfb459a9f2d0ef2249c
andersonmourot <109611768+andersonmourot@users.noreply.github.com>
2023-01-23 11:32:40 -0600

    Added findAll and Update functions

--- db/index.js
+++ db/index.js
@@ -5,16 +5,47 @@ class DB {
         this.connection = connection;
     }
 
+    createD(department) {
+        return this.connection.promise().query('INSTERT INTO department SET ?', department);
+    }
+    
     createE(employee) {
         return this.connection.promise().query('INSERT INTO employee SET ?', employee);
     }
 
-    createD(department) {
-        return this.connection.promise().query('INSTERT INTO department SET ?', department);
-    }
-
     createR(role) {
         return this.connection.promise().query('INSERT INTO role SET ?', role);
+    }
+
+    removeD(departmentID) {
+        return this.connection.promise().query('DELETE FROM department WHERE id = ?', departmentID);
+    }
+
+    removeE(employeeID) {
+        return this.connection.promise().query('DELETE FROM employee WHERE id = ?', employeeID);
+    }
+
+    removeR(roleID) {
+        return this.connection.promise().query('DELETE FROM role WHERE id = ?', roleID);
+    }
+
+    findAllD() {
+        return this.connection.promise().query('SELECT department.id, department.name FROM department;');
+    }
+
+    findAllE() {
+        return this.connection.promise().query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = departmnet.id LEFT JOIN employee manager on manager.id = employee.manager_id;");
+    }
+    findAllR() {
+        return this.connection.promise().query('SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id;');
+    }
+
+    updateEmployeeM(employeeId, managerId) {
+        return this.connection.promise().query('UPDATE employee SET manager_id = ? WHERE id = ?', [managerId, employeeId]);
+    }
+
+    updateEmployeeR(employeeId, roleId) {
+        return this.connection.promise().query('UPDATE employee SET role_id = ? WHERE id = ?' [roleId, employeeId]);
     }
 
     
