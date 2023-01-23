1bd8728d2b8e28062ce6d46939ca7d1728ed8536
andersonmourot <109611768+andersonmourot@users.noreply.github.com>
2023-01-23 12:40:52 -0600

    Added View and Add functionality

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const database = reuquire('./db');
+const db = reuquire('./db');
 const logo = require('asciiart-logo');
+const { ADDRCONFIG } = require('dns');
 const { prompt } = require('inquirer');
+const { removeListener } = require('process');
 require('console.table');
 init();
 
@@ -79,5 +81,106 @@ function loadPrompts() {
                 },
             ]
         }
-    ])
+    ]).then(res => {
+        let choice = res.choice;
+        switch (choice) {
+            case 'VIEW_EMPLOYEES_BY_DEPARTMENT':
+                viewEByD();
+                break;
+            case 'VIEW_EMPLOYEES_BY_MANAGER':
+                viewEByM();
+                break;
+            case 'VIEW_EMPLOYEES':
+                viewE();
+                break;
+            case 'ADD_EMPLOYEE':
+                addE();
+                break;
+            case 'UPDATE_EMPLOYEE_MANAGER':
+                updateEmployeeM();
+                break;
+            case 'UPDATE_EMPLOYEE_ROLE':
+                updateEmployeeR();
+                break;
+            case 'REMOVE_EMPLOYEE':
+                removeE();
+                break;
+            case 'VIEW_DEPARTMENTS':
+                viewD();
+                break;
+            case 'ADD_DEPARTMENT':
+                addD();
+                break;
+            case 'REMOVE_DEPARTMENT':
+                removeD();
+                break;
+            case 'VIEW_UTILIZED_BUDGET_BY_DEPARTMENT':
+                viewUtilizedBudgetByD();
+                break;
+            case 'VIEW_ROLES':
+                viewR();
+                break;
+            case 'ADD_ROLE':
+                addR();
+                break;
+            case 'REMOVE_ROLE':
+                removeR();
+                break;
+            default:
+                quit();
+        }
+    })
+}
+
+
+function viewR() {
+    db.findAllR().then(([rows]) => {
+        let roles = rows;
+        console.log('\n');
+        console.table(roles);
+    }).then(() => loadPrompts());
+}
+
+function viewD() {
+    db.findAllD().then(([rows]) => {
+        let departments = rows;
+        console.log('\n');
+        console.table(departments);
+    }).then(() => loadPrompts());
+}
+
+function viewE() {
+    db.findAllE().then(([rows]) => {
+        let employees = rows;
+        console.log('\n');
+        console.table(employees);
+    }).then(() => loadPrompts());
+}
+
+function addR() {
+    db.findAllD().then(([rows]) => {
+        let departments = rows;
+        const dChoices = departments.map(({ id, name }) => ({
+            name: name,
+            value: id
+        }));
+        prompt([
+            {
+                name: 'title',
+                message: 'What is the name of the role?'
+            },
+            {
+                name: 'salary',
+                message: 'What is the salary of the role?'
+            },
+            {
+                type: 'list',
+                name: 'department_id',
+                message: 'Which department does the role belong to?'
+            }
+        ])
+            .then(role => {
+                db.createR(role).then(() => console.log(`Added ${role.title} to the database`)).then(() => loadPrompts())
+            })
+    })
 }
