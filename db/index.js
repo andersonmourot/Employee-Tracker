c3ce6e35db94727e1cdba28656376b0b403bfe9c
andersonmourot <109611768+andersonmourot@users.noreply.github.com>
2023-01-23 11:43:37 -0600

    Added final functions

--- db/index.js
+++ db/index.js
@@ -48,5 +48,21 @@ class DB {
         return this.connection.promise().query('UPDATE employee SET role_id = ? WHERE id = ?' [roleId, employeeId]);
     }
 
-    
+    findAllEmployeesByD(department) {
+        return this.connection.promise().query('SELECT employee.id, employee.first_name, employee.last_name, role.title FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id WHERE department.id = ?;', departmentId);
+    }
+
+    findAllEmployeesByM(managerId) {
+        return this.connection.promise().query('SELECT employee.id, employee.first_name, employee.last_name, department.anme AS departmnet, role.title FROM employee LEFT JOIN role on role.id = employee.role_id LEFT JOIN department ON department.id = role.department_id WHERE manager_id = ?;', managerId);
+    }
+
+    findAllPossibleM(employeeId) {
+        return this.connection.promise().query('SELECT id, first_name, last_name, FROM employee WHERE id != ?', employeeId);
+    }
+
+    viewDBudgets() {
+        return this.connection.promise().query('SELECT department.id, department.name, SUM(role.salary) AS utilized_budget FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id GROUP BY department.id, department.name;');
+    }
 }
+
+module.exports = new DB(connection);
